d0ab15137c9deda34550c5ed247f5f1e26b48ba4
Alden Green <ajgreen@andrew.cmu.edu>
2016-11-14 18:03:39 -0500

    cutting sticks algorithm implemented via DP, unit tests supplied

--- Hwk4/hwk2.py
+++
@@ -1,9 +0,0 @@
-#input : document--file of text
-#output: words--list of all words in order of appearance
-def tokenize(document):
-	words = []
-	file = open(document,'r')
-	lines = file.readlines()
-	for line in lines:
-		words.extend(line.split())
-	return(words)
--- graph_traversal/traversal-tests.bat
+++
@@ -1,3 +0,0 @@
-python2 graph_traversal.py
-python2 graph_traversal_unittest.py
-
