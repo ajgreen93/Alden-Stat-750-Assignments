49035ec02cfddebacd9288e580c26db6bac219a8
Alden Green <ajgreen@andrew.cmu.edu>
2016-12-14 19:07:32 -0500

    Added example from the exericse to tests

--- cutting_sticks/best-cuts-tests.py
+++ cutting_sticks/best-cuts-tests.py
@@ -3,11 +3,14 @@ from cutting_sticks import *
 
 class tokenizeTests(unittest.TestCase):
 
-	def test_order(self):
-		self.assertEqual(best_cuts_complete(10,[4,2,7]),best_cuts_complete(10,[2,4,7]))
+    def test_order(self):
+        self.assertEqual(best_cuts_complete(10,[4,2,7]),best_cuts_complete(10,[2,4,7]))
 	
-	def test_empty(self):
-		self.assertEqual(best_cuts_complete(10,[]),[])
+    def test_empty(self):
+        self.assertEqual(best_cuts_complete(10,[]),[])
+    
+    def test_given_example(self):
+        self.assertEqual(best_cuts_complete(10,[2,4,7]),[4,2,7])
 		
 if __name__ == '__main__':
 	unittest.main()
